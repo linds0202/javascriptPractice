c86a796b2cf0e350cdf8f85689323f1ad8d68f15
linds0202 <lindsayarainey@gmail.com>
2022-07-04 11:57:13 -0700

    initial commit

---
+++ counter/app.js
@@ -0,0 +1,30 @@
+let count = 0
+
+const value = document.querySelector('#value')
+const increase = document.querySelector('.increase')
+const decrease = document.querySelector('.decrease')
+const reset = document.querySelector('.reset')
+
+increase.addEventListener('click', function(e) {
+    const actions = e.currentTarget.classList
+    if(actions.contains('increase')) {
+        count++
+    }
+    value.textContent = count
+})
+
+decrease.addEventListener('click', function(e) {
+    const actions = e.currentTarget.classList
+    if(actions.contains('decrease')) {
+        count--
+    }
+    value.textContent = count
+})
+
+reset.addEventListener('click', function(e) {
+    const actions = e.currentTarget.classList
+    if(actions.contains('reset')) {
+        count = 0
+    }
+    value.textContent = count
+})
